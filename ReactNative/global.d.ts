fc5e613416baef9aea6b20c315eaaa6ba51e42c9
Mike / iznogoud / fierc3 <mpullen9@gmail.com>
2023-11-20 23:56:06 +0100

    Prettify login screen

---
+++ ReactNative/global.d.ts
@@ -0,0 +1,8 @@
+declare module '*.png' {
+    const value: import('react-native').ImageSourcePropType;
+    export default value;
+  }
+  declare module '*.mp4' {
+    const value: import('react-native').AVPlaybackSource;
+    export default value;
+  }
