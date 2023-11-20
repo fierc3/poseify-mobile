3a45a8b8ffa8fbc2214d0b2233f84a92ac9f5206
Mike <mpullen9@gmail.com>
2023-11-20 22:19:53 +0100

    Add email contact in settings page (#34)
    Created a new hook which uses the native email client.

--- ReactNative/pages/settings.tsx
+++ ReactNative/pages/settings.tsx
@@ -1,18 +1,24 @@
 import React, { } from 'react';
 import { Appbar, Button } from 'react-native-paper';
 import { useAccessToken } from '../hooks/use-access-token';
+import { useEmail } from '../hooks/use-email';
+import { View } from 'react-native';
 
 
 export const Settings: React.FC = () => {
     console.log("|| Settings")
     const { logout } = useAccessToken();
+    const { sendDevInquiry } = useEmail();
 
     return (
         <>
             <Appbar.Header style={{ display: 'flex', flexDirection: 'column' }}>
                 <Appbar.Content title="Settings" style={{ justifyContent: 'center' }} />
             </Appbar.Header>
-            <Button icon="logout" onPress={() => logout()}> Logout </Button>
+            <View style={{height: '100%', justifyContent: 'space-evenly'}}>
+                <Button icon="logout" onPress={() => logout()}> Logout </Button>
+                <Button icon="email-fast" onPress={() => sendDevInquiry()}> Send Email to Devs </Button>
+            </View>
         </>
     );
 };
