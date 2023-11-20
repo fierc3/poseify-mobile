f47c50e00b7919809b2ce21e601e9da2aba40047
Mike / iznogoud / fierc3 <mpullen9@gmail.com>
2023-11-20 21:58:12 +0100

    Add functionality to delete animations

--- ReactNative/components/preview/viewer.tsx
+++ ReactNative/components/preview/viewer.tsx
@@ -4,13 +4,18 @@ import { Model } from './model';
 import { AttachmentType, IEstimation } from '../../helpers/api.types';
 import { useEffect, useState } from 'react';
 import { View } from 'react-native';
-import { getBvh } from '../../helpers/api';
+import { deleteAnimation, getBvh } from '../../helpers/api';
 import { useAccessToken } from '../../hooks/use-access-token';
 import { useNav } from '../../hooks/use-nav';
 import { ShareFileButton } from '../ui/buttons/shareFileButton';
 import { Appbar, Chip } from 'react-native-paper';
 import { TextSpinner } from '../ui/loading/textSpinner';
 import { getIconName } from '../../helpers/tags';
+import { ConfirmApiDialog } from '../ui/confirm/confirmApiDialog';
+import React from 'react';
+
+
+const MemoizedConfirmDialog = React.memo(ConfirmApiDialog);
 
 
 export type Props = {
@@ -24,6 +29,7 @@ export const Viewer: React.FC<Props> = ({ estimation }) => {
     const { accessToken } = useAccessToken();
     const { setEstimation } = useNav();
     const [isLoaded, setIsLoaded] = useState<boolean>(false);
+    const [deleteVisible, setDeleteVisible] = useState<boolean>(false);
 
     const camera = new THREE.PerspectiveCamera(80)
     camera.position.set(1, 2, 4)
@@ -52,8 +58,20 @@ export const Viewer: React.FC<Props> = ({ estimation }) => {
                 <Appbar.Header>
                     <Appbar.BackAction onPress={() => (setEstimation(null), setBvhData(null), setIsLoaded(false))} />
                     <Appbar.Content title={estimation.displayName} />
+                    <Appbar.Action icon={'delete'} onPress={() => setDeleteVisible(true)} />
                 </Appbar.Header>
-                {!isLoaded && (<TextSpinner label='Loading Your Animation! 🎬' />)}</>
+                {!isLoaded && (<TextSpinner label='Loading Your Animation! 🎬' />)}
+                <MemoizedConfirmDialog
+                    apiCall={deleteAnimation}
+                    apiProps={[accessToken.accessToken, estimation.internalGuid]}
+                    visible={deleteVisible}
+                    hideDialog={() => setDeleteVisible(false)}
+                    startButtonText='Delete'
+                    text='Delete this animation?'
+                    apiDoneText='Animation is deleted!'
+                    apiLoadingText='Deleting your animation...'
+                />
+            </>
             )}
 
             <View style={{ flex: 1, justifyContent: 'space-evenly', alignItems: 'center', display: estimation === null || bvhData === null ? 'none' : 'flex' }}>
@@ -61,7 +79,6 @@ export const Viewer: React.FC<Props> = ({ estimation }) => {
                     camera={camera}
                     gl={{ antialias: true }}
                     onCreated={(state) => {
-                        console.log("created");
                         state.scene.background = new THREE.Color("#f0f0f0");
                         state.scene.add(new THREE.GridHelper(5, 10));
                         const _gl = state.gl.getContext();
@@ -75,14 +92,14 @@ export const Viewer: React.FC<Props> = ({ estimation }) => {
                         setIsLoaded(true);
                     }
                     }
-                    style={{ width: "100%", maxHeight: "50%", height: "50%", display: estimation === null || bvhData === null ? 'none' : 'flex' }}>
+                    style={{ width: "100%", maxHeight: "50%", height: "50%", display: estimation === null || bvhData === null ? 'none' : 'flex', zIndex: -1 }}>
                     <ambientLight />
                     <pointLight position={[10, 10, 10]} />
                     <Model bvhData={bvhData} />
                 </Canvas>
                 {estimation && isLoaded && (
                     <View style={{ flexGrow: 0.5, justifyContent: 'space-evenly', alignItems: 'center', width: '100%' }}>
-                        <View style={{ flexGrow: 1, flexDirection: 'row', justifyContent: 'space-evenly', alignItems: 'center', width: '100%', gap: 4 }}>
+                        <View style={{ flexGrow: 1, flexDirection: 'row', justifyContent: 'space-evenly', alignItems: 'center', width: '100%', gap: 4, zIndex: -1 }}>
                             {   // workaround for tags bug
                                 (estimation?.tags[0] ?? '').split(",").filter(tag => tag.length > 0).map((tag, i) => <Chip key={i} icon={getIconName(tag)}>{tag}</Chip>)
                             }
