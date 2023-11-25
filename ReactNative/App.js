08c974031b2675654e50c68594192201a383ac2b
Mike / iznogoud / fierc3 <mpullen9@gmail.com>
2023-11-25 17:19:43 +0100

    Remove log suppression

--- ReactNative/App.js
+++ ReactNative/App.js
@@ -33,12 +33,6 @@ export default function App() {
   const { getEstimations } = useEstimations();
   const { getEstimation, getCurrentPage, setCurrentPage } = useNav();
 
-  // Generally a lot of issues with ThreeJs and expo apparently (recent issues: https://github.com/pmndrs/react-three-fiber/discussions/2823)
-  // Thus we ignore some logs
-  LogBox.ignoreLogs(['Cannot read property \'getX\' of undefined', // this error happens always, not clean but we ignore it so that expo doesn't show a popup
-    'Scripts "build/three.js" and "build/three.min.js" are deprecated with r150+' // can't fix this, issue is open
-  ]);
-
   const theme = {
     ...DefaultTheme,
     colors: {
