b99ce4e97c0e2f2b34c2709f5a5ba99b54e75c62
Mike <mpullen9@gmail.com>
2023-11-26 23:49:29 +0100

    Add hints viewer to main pages (#44)
    Opens the github wiki inside of the mobile app at the correct header.

--- ReactNative/pages/settings.tsx
+++ ReactNative/pages/settings.tsx
@@ -3,6 +3,7 @@ import { Appbar, Button } from 'react-native-paper';
 import { useAccessToken } from '../hooks/use-access-token';
 import { useEmail } from '../hooks/use-email';
 import { View } from 'react-native';
+import * as WebBrowser from 'expo-web-browser';
 
 
 export const Settings: React.FC = () => {
@@ -15,9 +16,10 @@ export const Settings: React.FC = () => {
             <Appbar.Header style={{ display: 'flex', flexDirection: 'column' }}>
                 <Appbar.Content title="Settings" style={{ justifyContent: 'center' }} />
             </Appbar.Header>
-            <View style={{height: '100%', justifyContent: 'space-evenly'}}>
-                <Button icon="logout" onPress={() => logout()}> Logout </Button>
+            <View style={{ height: '100%', justifyContent: 'space-evenly' }}>
+                <Button icon="help-circle-outline" onPress={() => WebBrowser.openBrowserAsync('https://github.com/fierc3/poseify/wiki/Poseify-Mobile-%E2%80%90-Quickstart')}> View Quickstart Guide </Button>
                 <Button icon="email-fast" onPress={() => sendDevInquiry()}> Send Email to Devs </Button>
+                <Button icon="logout" onPress={() => logout()}> Logout </Button>
             </View>
         </>
     );
