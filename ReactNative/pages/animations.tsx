dc05e8ed6d0ad1ce8ff46780adbe58ed5b68dafa
Mike <mpullen9@gmail.com>
2023-12-04 20:45:18 +0100

    Add screen when 0 estimations are found (#57)
    When estiamtions list is empty and not null, we display a new info
    screen. The screen hints add starting a recording to create animations.

--- ReactNative/pages/animations.tsx
+++ ReactNative/pages/animations.tsx
@@ -1,9 +1,10 @@
 import React, { } from 'react';
-import { Appbar } from 'react-native-paper';
+import { Appbar, Icon, Text } from 'react-native-paper';
 import { AnimationsList } from '../components/ui/list/animationsList';
 import { TextSpinner } from '../components/ui/loading/textSpinner';
 import { useEstimations } from '../hooks/use-estimations';
 import * as WebBrowser from 'expo-web-browser';
+import { View } from 'react-native';
 
 
 const MemoizedAnimationsList = React.memo(() => {
@@ -18,10 +19,15 @@ export const Animations: React.FC = () => {
         <>
             <Appbar.Header style={{ display: 'flex', flexDirection: 'column' }}>
                 <Appbar.Content title="Animations" style={{ justifyContent: 'center' }} />
-                <Appbar.Action size={23} style={{position: 'absolute', right: 20, top: 5}} icon="help-circle-outline" onPress={() => WebBrowser.openBrowserAsync('https://github.com/fierc3/poseify/wiki/Poseify-Mobile-%E2%80%90-Quickstart#animations-list')} />
+                <Appbar.Action size={23} style={{ position: 'absolute', right: 20, top: 5 }} icon="help-circle-outline" onPress={() => WebBrowser.openBrowserAsync('https://github.com/fierc3/poseify/wiki/Poseify-Mobile-%E2%80%90-Quickstart#animations-list')} />
             </Appbar.Header>
             <MemoizedAnimationsList />
             {getEstimations() === null && (<TextSpinner label='Looking for your animations!' styleContainer={{ height: '100%', width: '100%', position: 'absolute' }} />)}
+            {getEstimations()?.length === 0 &&
+                <View style={{ opacity: 0.6, gap: 10, height: '100%', width: '100%', position: 'absolute', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                    <Icon size={60} source={"camera-plus"} />
+                    <Text variant='bodyLarge'>No animations created yet, try recording one!</Text>
+                </View>}
         </>
     );
 };
